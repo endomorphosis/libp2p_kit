27977de872bbfe1c3e19c816dd0799c3753a9d38
endomorphosis <starworks5@gmail.com>
2024-09-03 23:00:32 -0700

    chore: Update libp2p_kit import statements

--- libp2p_kit_py/libp2p_kit.py
+++ libp2p_kit_py/libp2p_kit.py
@@ -1,8 +1,8 @@
-from libp2p_kit import libp2p_kit
-from aria2_kit import aria2_kit
-from s3_kit import s3_kit
-from websocket_kit import websocket_kit
-from websocket_kit import websocket_kit_lib
+from .libp2p_kit import libp2p_kit
+from .aria2_kit import aria2_kit
+from .s3_kit import s3_kit
+from .websocket_kit import websocket_kit
+from .websocket_kit import websocket_kit_lib
 
 class libp2p_kit_py:
     def __init__(self, resources, metadata):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='lihp2p_kit_py',
-	version='0.0.9',
+	version='0.0.10',
 	packages=[
 		'libp2p_kit_py',
 	],
